38e5d4d26efe54abc38a425725439020dcc1cec6
Tyler <tbjohns10@gmail.com>
2014-03-12 15:48:01 -0700

    Fixed an issue with the phrases path and launchd

--- shoes.rb
+++ shoes.rb
@@ -4,125 +4,126 @@ require "nike_v2"
 
 module Shoes
 
-	def Shoes.get_tweet
-		info = Shoes.get_nike_info()
+  def Shoes.get_tweet
+    info = Shoes.get_nike_info()
 
-		if Shoes.new_info? info
-			return Shoes.cocky_brag info
-		else
-			return Shoes.lazy_dis info
-		end
-	end
+    if Shoes.new_info? info
+      return Shoes.cocky_brag info
+    else
+      return Shoes.lazy_dis info
+    end
+  end
 
-	private
+  private
 
-		def Shoes.get_nike_info
-			creds_path = File.expand_path("../config/nike_creds.yml", __FILE__)
-			nike_creds = YAML.load_file(creds_path)
-			me = NikeV2::Person.new(access_token: nike_creds[:access_token])
-			me.summary.running
+    def Shoes.get_nike_info
+      creds_path = File.expand_path("../config/nike_creds.yml", __FILE__)
+      nike_creds = YAML.load_file(creds_path)
+      me = NikeV2::Person.new(access_token: nike_creds[:access_token])
+      me.summary.running
 
-			info = {}
-			km_to_miles = 0.621371  
+      info = {}
+      km_to_miles = 0.621371  
 
-			summary = me.summary.running
-			total_duration = summary["LIFETIMEDURATION"]
-			info[:total_hours] = total_duration.split(":")[0]
-			total_distance = summary["LIFETIMEDISTANCE"]
-			info[:total_miles] = Float(total_distance) * km_to_miles
+      summary = me.summary.running
+      total_duration = summary["LIFETIMEDURATION"]
+      info[:total_hours] = total_duration.split(":")[0]
+      total_distance = summary["LIFETIMEDISTANCE"]
+      info[:total_miles] = Float(total_distance) * km_to_miles
 
-			latest_run = me.activities(activity_type: "RUN").first
-			info[:latest_time] = latest_run.start_time
-			info[:latest_id] = latest_run.activity_id
-			metrics = latest_run.metric_summary
-			(hours, minutes, secs) = metrics["duration"].split(":")
-			info[:latest_minutes] = 60*Integer(hours) + Integer(minutes)
-			info[:latest_miles] = metrics["distance"] * km_to_miles
+      latest_run = me.activities(activity_type: "RUN").first
+      info[:latest_time] = latest_run.start_time
+      info[:latest_id] = latest_run.activity_id
+      metrics = latest_run.metric_summary
+      (hours, minutes, secs) = metrics["duration"].split(":")
+      info[:latest_minutes] = 60*Integer(hours) + Integer(minutes)
+      info[:latest_miles] = metrics["distance"] * km_to_miles
 
-			info
-		end
+      info
+    end
 
-		def Shoes.new_info? info
-			return !Shoes.note_match("latest_run_id", info[:latest_id])
-		end
+    def Shoes.new_info? info
+      return !Shoes.note_match("latest_run_id", info[:latest_id])
+    end
 
-		def Shoes.random_phrase type
-			f = File.open "phrases/#{type}.txt"
-			phrase_ar = []
-			f.each_line{ |l| phrase_ar.push l.strip }
-			phrase_ar.sample
-		end
+    def Shoes.random_phrase type
+      path = File.expand_path("../phrases/#{type}.txt", __FILE__)
+      f = File.open path
+      phrase_ar = []
+      f.each_line{ |l| phrase_ar.push l.strip }
+      phrase_ar.sample
+    end
 
-		def Shoes.cocky_brag info
-			base = ""
-			r = rand
-			if r < 0.25
-				base += "Went"
-			else
-				base += "Ran"
-			end
-			
-			r = rand
-			if r < 0.6
-				base += " #{"%0.1f" % info[:latest_miles]} miles today"
-			else
-				base += " #{info[:latest_minutes]} minutes today"
-			end
+    def Shoes.cocky_brag info
+      base = ""
+      r = rand
+      if r < 0.25
+        base += "Went"
+      else
+        base += "Ran"
+      end
+      
+      r = rand
+      if r < 0.6
+        base += " #{"%0.1f" % info[:latest_miles]} miles today"
+      else
+        base += " #{info[:latest_minutes]} minutes today"
+      end
 
-			r = rand
-			if r < 0.4
-				base += "!"
-			else
-				base += "."
-			end
-		
-			hashtag = Shoes.random_phrase "positive_hashtags"
+      r = rand
+      if r < 0.4
+        base += "!"
+      else
+        base += "."
+      end
+    
+      hashtag = Shoes.random_phrase "positive_hashtags"
 
-			r = rand
-			return "#{base} #{hashtag}"
-		end
+      r = rand
+      return "#{base} #{hashtag}"
+    end
 
-		def Shoes.lazy_dis info
-			seconds_passed = (Time.now - Time.parse(info[:latest_time]))
-			days_passed = Integer(seconds_passed/24/60/60)
+    def Shoes.lazy_dis info
+      seconds_passed = (Time.now - Time.parse(info[:latest_time]))
+      days_passed = Integer(seconds_passed/24/60/60)
 
-			description = "Been a "
-			if days_passed < 7
-				Shoes.note_match("last_dig", days_passed) # reset note file
-				return nil
-			elsif days_passed == 7
-				return nil if Shoes.note_match("last_dig", days_passed)	
-				description += "week"
-			elsif days_passed == 30
-				return nil if Shoes.note_match("last_dig", days_passed)	
-				description += "month"
-			elsif days_passed == 365
-				return nil if Shoes.note_match("last_dig", days_passed)	
-				description += "year"
-			else
-				return nil
-			end
-			description += " since the last run."
-		
-			comment = Shoes.random_phrase "negative_comments"
-			hashtag = Shoes.random_phrase "negative_hashtags"
-			return "#{description} #{comment} #{hashtag}"
-		end
+      description = "Been a "
+      if days_passed < 7
+        Shoes.note_match("last_dig", days_passed) # reset note file
+        return nil
+      elsif days_passed == 7
+        return nil if Shoes.note_match("last_dig", days_passed) 
+        description += "week"
+      elsif days_passed == 30
+        return nil if Shoes.note_match("last_dig", days_passed) 
+        description += "month"
+      elsif days_passed == 365
+        return nil if Shoes.note_match("last_dig", days_passed) 
+        description += "year"
+      else
+        return nil
+      end
+      description += " since the last run."
+    
+      comment = Shoes.random_phrase "negative_comments"
+      hashtag = Shoes.random_phrase "negative_hashtags"
+      return "#{description} #{comment} #{hashtag}"
+    end
 
-		def Shoes.note_match name, contents
-			contents = contents.to_s()
-			path = File.expand_path("../notes/#{name}", __FILE__)
-			if File.exists? path
-				f = File.open path
-				if f.read == contents
-					return true
-				end
-			end
+    def Shoes.note_match name, contents
+      contents = contents.to_s()
+      path = File.expand_path("../notes/#{name}", __FILE__)
+      if File.exists? path
+        f = File.open path
+        if f.read == contents
+          return true
+        end
+      end
 
-			f = File.open path, "w"
-			f.write contents
-			f.close()
-			return false
-		end
+      f = File.open path, "w"
+      f.write contents
+      f.close()
+      return false
+    end
 
 end
--- twitterface.rb
+++ twitterface.rb
@@ -4,19 +4,19 @@ require "yaml"
 
 module Twitterface 
 
-	creds_path = File.expand_path("../config/twitter_creds.yml", __FILE__)
-	twitter_creds = YAML.load_file(creds_path)
+  creds_path = File.expand_path("../config/twitter_creds.yml", __FILE__)
+  twitter_creds = YAML.load_file(creds_path)
 
-	client = Twitter.configure do |config|
-		config.consumer_key = twitter_creds[:consumer_key]
-		config.consumer_secret = twitter_creds[:consumer_secret]
-		config.oauth_token = twitter_creds[:oauth_token]
-		config.oauth_token_secret = twitter_creds[:oauth_token_secret]
-	end
+  client = Twitter.configure do |config|
+    config.consumer_key = twitter_creds[:consumer_key]
+    config.consumer_secret = twitter_creds[:consumer_secret]
+    config.oauth_token = twitter_creds[:oauth_token]
+    config.oauth_token_secret = twitter_creds[:oauth_token_secret]
+  end
 
-	def Twitterface.send_tweet(tweet)
-		Twitter.update(tweet)
-	end
+  def Twitterface.send_tweet(tweet)
+    Twitter.update(tweet)
+  end
 
 end
 
